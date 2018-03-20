0b64b694e13f29e8989680722b7fa399a9ed92e8
cnwobi <cinwobi@gmail.com>
2018-03-20 13:01:47 +1100

    AOP implement

--- src/com/buck/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/buck/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
-/*package com.buck.springdemo.aspect;
+package com.buck.springdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,24 +14,26 @@ import java.util.logging.Logger;
 public class CRMLoggingAspect {
     //setup logger
     private Logger logger= Logger.getLogger(getClass().getName());
-    //setup pointcut declaratiom
-    @Pointcut("execution(* com.buck.springdemo.controller .* .*(..))")
+    //setup pointcut declaration
+
+  @Pointcut("execution(* com.buck.springdemo.controller ..*.*(..))")
     private void forControllerPackage(){
 
     }
-    @Pointcut("execution(* com.buck.springdemo.services. * .*(..))")
+    @Pointcut("execution(* com.buck.springdemo.services ..*.*(..))")
     private void forServicesPackage(){
 
     }
-    @Pointcut("execution(* com.buck.springdemo.dao. * .*(..))")
-    private void forDAOPackage(){
+    @Pointcut("execution(* com.buck.springdemo.dao ..*.*(..))")
+    private void forDaoPackage(){
 
     }
+   @Pointcut("forControllerPackage()|| forServicesPackage()||forDaoPackage()")
+   private void forAppFlow(){
 
-    @Pointcut("forControllerPackage()|| forDAOPackage()|| forServicesPackage()")
-    private void forAppFlow(){
+   }
 
-    }
+
     //add @Before advice
     @Before("forAppFlow()")
     public  void before(JoinPoint joinPoint){
@@ -38,7 +41,22 @@ public class CRMLoggingAspect {
         String method=joinPoint.getSignature().toShortString();
         logger.info("======>> in @Before: calling method: " +method);
         // display arguments to the method
+//get the arguments
+        Object [] args =joinPoint.getArgs();
+        // loop and display
+        for(Object tempArg:args){
+            logger.info("====>> argumet: " +tempArg);
+        }
+    }
+    //add @after returning advice
+@AfterReturning(pointcut = "forAppFlow()",returning = "result")
+    public void afterReturning(JoinPoint joinPoint,Object result){
+      //display method returning
+    String method=joinPoint.getSignature().toShortString();
+    logger.info("======>> in @AfterReturning: calling method: " + method);
+    //display data returned
+    logger.info("====>> result: " + result);
 
     }
-    //add @AfterReturning advice
-}*/
+
+}
